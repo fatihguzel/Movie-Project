56e94968297565899c2892848cdeb2fd3af00e2a
fatihguzel <98105601+fatihguzel@users.noreply.github.com>
2022-05-24 18:35:37 +0300

    Update ui.js

--- Movie Project/ui.js
+++ Movie Project/ui.js
@@ -32,3 +32,17 @@ UI.prototype.displayMessages = function (message,type) {
       div.remove()  
     },1000)
 }
+UI.prototype.loadAllFilms = function(films){
+    const filmList = document.getElementById("films")
+
+    films.forEach(function (film) {
+        filmList.innerHTML += `
+        <tr>
+        <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+        <td>${film.title}</td>
+        <td>${film.director}</td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+      </tr>
+        `
+    })
+}
