6c84357198d05f272366b255cda26c3ee5178ad9
fatihguzel <98105601+fatihguzel@users.noreply.github.com>
2022-05-25 14:31:42 +0300

    Update storage.js

--- Movie Project/storage.js
+++ Movie Project/storage.js
@@ -20,3 +20,16 @@ Storage.prototype.getFilmFromStorage = function () {
     }
     return films
 }
+Storage.prototype.deleteFilmFromStorage = function (filmTitle) {
+    let films = this.getFilmFromStorage()
+
+    films.forEach(function(film,index){
+        if(film.title === filmTitle){
+            films.splice(index,1)
+        }
+    })
+    localStorage.setItem("films",JSON.stringify(films))
+}
+Storage.prototype.clearAllFilmsFromStorage = function () {
+    localStorage.removeItem("films")
+}
